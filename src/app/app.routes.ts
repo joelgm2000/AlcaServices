019e0e546de4c9ca5209fcc964c561c84347568f
joelgm2000 <53059969+joelgm2000@users.noreply.github.com>
2024-10-28 22:41:57 -0500

    ica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ImpuestoPredialUnificadoComponent } from '../EstatutoTributario022_2022/impuesto-predial-unificado/impuesto-predial-unificado.component';
+import { ImpuestoIndustriaYComercioComponent } from '../EstatutoTributario022_2022/impuesto-industria-ycomercio/impuesto-industria-ycomercio.component';
 
 export const routes: Routes = [
     { path: 'impuesto-predial-unificado', component: ImpuestoPredialUnificadoComponent },
+    { path: 'impuesto-industria-y-comercio', component: ImpuestoIndustriaYComercioComponent },
+
 
     { path: '', redirectTo: 'impuesto-predial-unificado', pathMatch: 'full' } // Redirige a una ruta existente
 ];
